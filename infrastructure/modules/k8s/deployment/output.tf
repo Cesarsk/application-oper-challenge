011490ee984188d2e46e46dc257869d32dbf4744
Luca Cesarano <lu.cesarano@reply.it>
2022-04-16 19:38:32 +0200

    Split Application and Infrastructure Repository

--- infrastructure/modules/k8s/deployment/output.tf
+++
@@ -1,3 +0,0 @@
-output "kubernetes_deployment_python_webserver_metadata_name" {
-  value = kubernetes_deployment.python-webserver.metadata[0].name
-}
--- infrastructure/modules/k8s/service/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "python_webserver_deployment_labels_run" {
-  type = string
-}
